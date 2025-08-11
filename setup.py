e3931867cbe3e017ccbc95231628366e7f5469f5
Alan M Watson <alan.watson@pitt.edu>
2025-08-11 10:14:10 -0400

    name fixes

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 
 
 # https://github.com/pypa/setuptools_scm
-use_scm = {"write_to": "shortner/_version.py"}
+use_scm = {"write_to": "urlshortener/_version.py"}
 setup(use_scm_version=use_scm)
